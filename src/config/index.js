fbe1137fb591cb530bc9c081aabc5c470c172a2d
Alex_Lu <1004026475@qq.com>
2021-12-29 11:47:04 +0800

    dev url set as 127.0.0.1:7001/api

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     // 开发和打包是默认的两个环境
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://121.40.234.82:8001/web/api' : 'http://121.40.234.82:8001/web/api',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7001/api' : 'http://121.40.234.82:8001/web/api',
     admin: 'ADMIN',
 }
 
